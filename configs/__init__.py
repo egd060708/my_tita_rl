e726954a991987f8cbd0b09938dcc902fb72b167
lu <2231625449@qq.com>
2025-04-15 17:56:04 +0800

    移植tita_rl仓库

---
+++ configs/__init__.py
@@ -0,0 +1,10 @@
+from .base_config import *
+from .legged_robot_config import *
+from .tita_constraint_config import *
+from .tita_double_contact_config import *
+from .tita_single_contact_config import *
+
+from .tita_rough_config import TitaRoughCfg, TitaRoughCfgPPO
+from .tita_flat_config import TitaFlatCfg, TitaFlatCfgPPO
+
+from .tita_heading_config import TitaHeadingCfg, TitaHeadingCfgPPO
---
+++ envs/__init__.py
@@ -0,0 +1,5 @@
+from .base_task import BaseTask
+from .no_constrains_legged_robot import Tita
+from .legged_robot import LeggedRobot
+from .vec_env import VecEnv
+from .heading_legged_robot import HeadingLeggedRobot
